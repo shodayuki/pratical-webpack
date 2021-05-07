dbeccd2289a7b692c3bc40c0146eeb0dc665c085
shodayuki <yshoda1227@gmail.com>
2021-05-08 01:23:43 +0900

    キャッシュ対策を追加

--- webpack.common.js
+++ webpack.common.js
@@ -9,8 +9,8 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'js/[name].bundle.js',
-    chunkFilename: 'js/[name].js',
+    filename: 'js/[name].[contenthash].js',
+    chunkFilename: 'js/[name].[contenthash].js',
   },
   optimization: {
     splitChunks: {
