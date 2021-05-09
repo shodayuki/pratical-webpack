a103d384edfb6532a38dd6d677a2102bb50d92c8
shodayuki <yshoda1227@gmail.com>
2021-05-10 01:54:04 +0900

    npm install --save-dev eslint-loader@4.0.2 eslint@7.5.0 実行

--- webpack.common.js
+++ webpack.common.js
@@ -32,10 +32,19 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-      },
+      }
     ],
   },
   plugins: [
